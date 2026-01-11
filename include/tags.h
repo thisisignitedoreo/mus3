8e51abfba75a5d4e6133b9ca6ca0cffeff9b910e
aciddev <you@example.com>
2026-01-11 03:48:04 +0300

    Here we go!

--- include/tags.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "music.h"
-
-#include <allocator.h>
-
-struct music_song tags_load(struct allocator arena, const char *path);
--- src/media-backends/null.c
+++
@@ -1,9 +0,0 @@
-#include "media.h"
-
-bool media_setup(struct music_context *ctx) { return true; }
-
-enum media_event media_tick(void) { return EVENT_NONE; }
-
-void media_update(enum media_changes changes) { }
-
-void media_destroy(void) {}
